fa58b984a4b0c9c66fdbc20adfb65fa1f248ef8e
Irvanabdurrahman <irvan_chester@ymail.com>
2012-11-13 18:42:24 +0700

    Sesi-05

---
+++ Sesi-05/BlankArea.java
@@ -0,0 +1,30 @@
+/* Copyright (c) by Irvan Abdurrahman - 4510210003
+	--   Friday, November 09, 2012 | 10:00 PM
+*/
+
+/**
+	* BlankArea.java is used by :
+	* MouseEventDemo.java
+*/
+import javax.swing.*;
+import java.awt.Dimension;
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class BlankArea extends JLabel{
+	Dimension minSize = new Dimension(100, 50);
+	
+	public BlankArea(Color color){
+		setBackground(color);
+		setOpaque(true);
+		setBorder(BorderFactory.createLineBorder(Color.black));
+	}
+	
+	public Dimension getMinimumSize(){
+		return minSize;
+	}
+	
+	public Dimension getPreferredSize(){
+		return minSize;
+	}
+}
